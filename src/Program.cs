cc61632a555c5ddfdb1bc3bdc39035e08d21efff
adrianodts <adrianodts@msn.com>
2020-07-15 00:52:44 -0300

    Inclusao de models e aquivo sln

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AdvertApi.Models
+{
+    public class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
